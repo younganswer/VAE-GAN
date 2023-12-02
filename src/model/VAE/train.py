aa1930b5cad7020a94d26fdd3ff92138f1196352
Younganswer <dudtj1999@gmail.com>
2023-12-02 19:45:21 +0900

    [Fix] Adjust hyperparameters

--- src/model/VAE/train.py
+++ src/model/VAE/train.py
@@ -18,7 +18,7 @@ def train(train_loader, learning_rate=0.005, epochs=5):
 			data = data.to(device)
 			optimizer.zero_grad()
 			outputs = model(data)
-			result = criterion(*outputs, M_N=0.00001)
+			result = criterion(*outputs, M_N=0.00025)
 			loss = result['loss']
 			recon_loss = result['Reconstruction_Loss']
 			kl_loss = result['KLD']
