92428cca01bf25dda15f2924f79754a9d5893790
Younganswer <dudtj1999@gmail.com>
2023-12-05 00:58:29 +0900

    [Fix] Add noise to label

--- src/model/GAN/train.py
+++ src/model/GAN/train.py
@@ -84,7 +84,7 @@ def main():
 		drop_last=True,
 	)
 
-	model = train(train_loader, learning_rate=0.005, epochs=5)
+	model = train(train_loader, learning_rate=0.005, epochs=3)
 
 	torch.save(model.state_dict(), './src/model/GAN/CelebA_64_square.pth')
 
