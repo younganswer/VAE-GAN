9ffaa2b52d62942f605daa311c819ab543fe9175
Younganswer <dudtj1999@gmail.com>
2023-12-04 18:15:45 +0900

    [Fix] Modify discriminator loss

--- src/model/GAN/train.py
+++ src/model/GAN/train.py
@@ -23,7 +23,7 @@ def train(train_loader, learning_rate=0.005, epochs=5):
 			generator_optimizer.zero_grad()
 			generated_image = generator(model.sample(data.shape[0], device))
 			pred_fake = discriminator(generated_image)
-			generator_loss = 2 * generator.loss_function(pred_fake)
+			generator_loss = generator.loss_function(pred_fake)
 			generator_loss.backward()
 			generator_optimizer.step()
 			# ----------------------------------------------------------------------------------
@@ -32,7 +32,7 @@ def train(train_loader, learning_rate=0.005, epochs=5):
 			discriminator_optimizer.zero_grad()
 			pred_fake = discriminator(generated_image.detach())
 			pred_real = discriminator(data)
-			discriminator_loss = discriminator.loss_function(pred_fake, pred_real)
+			discriminator_loss = 10 * discriminator.loss_function(pred_fake, pred_real)
 			discriminator_loss.backward()
 			discriminator_optimizer.step()
 			# ----------------------------------------------------------------------------------
