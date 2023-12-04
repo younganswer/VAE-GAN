6e2be03d61e7b6c9f4e4113b6658acffc072385a
Younganswer <dudtj1999@gmail.com>
2023-12-04 22:05:22 +0900

    [Fix] Add noise to label

--- src/model/GAN/train.py
+++ src/model/GAN/train.py
@@ -37,21 +37,17 @@ def train(train_loader, learning_rate=0.005, epochs=5):
 			# Train real data
 			discriminator.zero_grad()
 			pred_real = discriminator(data)
-			# Flip label per 100 steps
-			#if (i + 1) % 100 == 0:
-				#real_loss = F.mse_loss(pred_real, fake_label)
-			#else:
 			real_loss = F.mse_loss(pred_real, real_label)
+			if (i + 1) % 100 == 0: # Flip label per 100 steps
+				real_loss = F.mse_loss(pred_real, fake_label)
 			real_loss.backward()
 
 			# Train fake data
 			fake = generator(model.sample(data.shape[0], device))
 			pred_fake = discriminator(fake.detach())
-			# Flip label per 100 steps
-			#if (i + 1) % 100 == 0:
-				#fake_loss = F.mse_loss(pred_fake, real_label)
-			#else:
 			fake_loss = F.mse_loss(pred_fake, fake_label)
+			if (i + 1) % 100 == 0: # Flip label per 100 steps
+				fake_loss = F.mse_loss(pred_fake, real_label)
 			fake_loss.backward()
 
 			discriminator_loss = real_loss + fake_loss / 2
