524d652c95b02a66f176f917abf213483eda3454
Younganswer <dudtj1999@gmail.com>
2023-12-04 22:00:02 +0900

    [Fix] Add noise to label

--- src/model/GAN/train.py
+++ src/model/GAN/train.py
@@ -27,7 +27,8 @@ def train(train_loader, learning_rate=0.005, epochs=5):
 			data = (data - 0.5) / 0.5
 
 			# Add noise to label			
-			noise_factor = 0.01
+			#noise_factor = 0.01
+			noise_factor = 0
 			noise = torch.randn(data.shape[0], 1, device=device) * noise_factor
 			real_label = torch.ones(data.shape[0], 1, device=device) + noise
 			fake_label = torch.zeros(data.shape[0], 1, device=device) + noise
