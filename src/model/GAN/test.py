99affbb7805d68252d7f49a63542b23e57e4fe99
Younganswer <dudtj1999@gmail.com>
2023-12-04 13:29:41 +0900

    [Fix] unittest

--- src/model/GAN/test.py
+++ src/model/GAN/test.py
@@ -9,23 +9,39 @@ class TestGAN(unittest.TestCase):
 	def setUp(self) -> None:
 		self.device = torch.device(0 if torch.cuda.is_available() else 'cpu')
 		self.model = GAN().to(self.device)
+		self.generator = self.model.generator
+		self.discriminator = self.model.discriminator
 
 	def test_summary(self):
 		print("Generator")
-		print(summary(self.model.generator, (10,)))
+		print(summary(self.generator, (128,)))
 		print("Discriminator")
-		print(summary(self.model.discriminator, (3, 224, 224)))
+		print(summary(self.discriminator, (3, 64, 64)))
 
-	def test_forward(self):
-		z = torch.randn(1, 10)
-		x = torch.randn(1, 3, 224, 224)
-		y = self.model(z, x)
+	def test_generator_forward(self):
+		z = torch.randn(1, 128)
+		x = self.generator(z)
+		self.assertEqual(x.shape, (1, 3, 64, 64))
 
-	def test_loss_function(self):
-		z = torch.randn(1, 10)
-		x = torch.randn(1, 3, 224, 224)
-		y = self.model(z, x)
-		loss = self.model.loss_function(*y)
+	def test_generator_loss_function(self):
+		z = torch.randn(1, 128)
+		x = self.generator(z)
+		pred_fake = self.discriminator(x)
+		loss = self.generator.loss_function(pred_fake)
+		self.assertEqual(loss.shape, ())
+
+	def test_discriminator_forward(self):
+		x = torch.randn(1, 3, 64, 64)
+		y = self.discriminator(x)
+		self.assertEqual(y.shape, (1, 1))
+
+	def test_discriminator_loss_function(self):
+		x = torch.randn(1, 3, 64, 64)
+		y = self.discriminator(x)
+		pred_fake = self.discriminator(x)
+		pred_real = self.discriminator(x)
+		loss = self.discriminator.loss_function(pred_fake, pred_real)
+		self.assertEqual(loss.shape, ())
 
 if __name__ == '__main__':
 	unittest.main()
