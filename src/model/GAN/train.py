fcad1ff1354d6406fff9241fd7e7beb1a172b080
Younganswer <dudtj1999@gmail.com>
2023-12-04 20:46:21 +0900

    [Fix] Deep Convolutional GAN with Least Squares

--- src/model/GAN/train.py
+++ src/model/GAN/train.py
@@ -28,9 +28,9 @@ def train(train_loader, learning_rate=0.005, epochs=5):
 			
 			# Set real_label with gaussian noise
 			noise_factor = 0.1
-			noise = torch.randn(data.shape[0], 1) * noise_factor
-			real_label = torch.ones(data.shape[0], 1) + noise
-			fake_label = torch.zeros(data.shape[0], 1) + 0.1 + noise
+			noise = torch.randn(data.shape[0], 1, device=device) * noise_factor
+			real_label = torch.ones(data.shape[0], 1, device=device) - noise
+			fake_label = torch.zeros(data.shape[0], 1, device=device) + noise
 
 			# Train Discriminator --------------------------------------------------------------
 			# Train real data
