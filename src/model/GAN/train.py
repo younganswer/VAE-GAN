ab9e642f2f7232d84ba8b634d8d332811bc3674d
Younganswer <dudtj1999@gmail.com>
2023-12-13 00:26:57 +0900

    [Fix] Remove noise

--- src/model/GAN/train.py
+++ src/model/GAN/train.py
@@ -54,28 +54,28 @@ def train(model, device, train_loader, learning_rate=0.005, epochs=5, noise_fact
 		for i, data in enumerate(train_loader):
 			data = data.to(device)
 
-			real_label = torch.ones(data.shape[0], 1, device=device)
-			fake_label = torch.zeros(data.shape[0], 1, device=device)
-			real_noise = torch.randn(data.shape[0], 1, device=device) * noise_factor
-			fake_noise = torch.randn(data.shape[0], 1, device=device) * noise_factor + 0.1
+			#real_label = torch.ones(data.shape[0], 1, device=device)
+			#fake_label = torch.zeros(data.shape[0], 1, device=device)
+			#real_noise = torch.randn(data.shape[0], 1, device=device) * noise_factor
+			#fake_noise = torch.randn(data.shape[0], 1, device=device) * noise_factor + 0.1
 
 			# Train Discriminator --------------------------------------------------------------
 			# Train real data
 			discriminator.zero_grad()
 			pred_real = discriminator(data)
-			real_loss = F.mse_loss(pred_real, real_label + real_noise)
+			real_loss = F.mse_loss(pred_real, real_label)
 			#if (i + 1) % 16 == 0: # Flip label per 16 steps
 			#	real_loss = F.mse_loss(pred_real, fake_label)
+			real_loss.backward()
 
 			# Train fake data
 			fake = generator(model.sample(data.shape[0], device))
 			pred_fake = discriminator(fake.detach())
-			fake_loss = F.mse_loss(pred_fake, fake_label + fake_noise)
+			fake_loss = F.mse_loss(pred_fake, fake_label)
 			#if (i + 1) % 16 == 0: # Flip label per 16 steps
 			#	fake_loss = F.mse_loss(pred_fake, real_label)
+			fake_loss.backward()
 
-			discriminator_loss = (real_loss + fake_loss) / 2
-			discriminator_loss.backward()
 			discriminator_optimizer.step()
 			# ----------------------------------------------------------------------------------
 
